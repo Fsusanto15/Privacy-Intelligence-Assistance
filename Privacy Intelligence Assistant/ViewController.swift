e8690ca9907e330791fa138b49e79889303e6641
Fsusanto15 <fsusanto15@gmail.com>
2026-05-08 11:21:36 +1000

    Initial Commit

---
+++ Privacy Intelligence Assistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Privacy Intelligence Assistant
+//
+//  Created by Felix B Susanto on 8/5/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Privacy Intelligence AssistantTests/Privacy_Intelligence_AssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  Privacy_Intelligence_AssistantTests.swift
+//  Privacy Intelligence AssistantTests
+//
+//  Created by Felix B Susanto on 8/5/2026.
+//
+
+import Testing
+@testable import Privacy_Intelligence_Assistant
+
+struct Privacy_Intelligence_AssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
